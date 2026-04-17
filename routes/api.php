adbc3bf7c5b6f8569e09374c00ba5d8a140afad9
ThanhTung204 <hoangtungls587@gmail.com>
2026-04-17 16:00:36 +0700

    Delete routes directory

--- routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\StudentController;
-
-Route::get('/students', [StudentController::class, 'index']);
-Route::post('/students', [StudentController::class, 'store']);
-Route::get('/students/{id}/subjects', [StudentController::class, 'subjects']);
-Route::post('/students/{id}/register-subject/{subjectId}', [StudentController::class, 'register']);
